fa78afdc402e33b50c903267bbe0ab9bb4d82b5a
PrashantShaw <prashant.shaw.3154@gmail.com>
2023-05-03 19:16:56 +0530

    fix: added del btn to edit modal

--- src/components/EditMOdal.jsx
+++ src/components/EditMOdal.jsx
@@ -16,7 +16,10 @@ const EditMOdal = ({ member, setMemberEditing, handleEditSave }) => {
                 <input type="text" value={member.name} name='name' onChange={handleOnChange} />
                 <input type="text" value={member.email} name='email' onChange={handleOnChange} />
                 <input type="text" value={member.role} name='role' onChange={handleOnChange} />
-                <button onClick={handleEditSave}>Save</button>
+                <div className="modal-btns">
+                    <button onClick={handleEditSave}>Save</button>
+                    <button onClick={() => { setMemberEditing(null) }}>Cancel</button>
+                </div>
             </div>
         </div>
     )
